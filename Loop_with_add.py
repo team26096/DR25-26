9ec924cf34e482058e21af10a76f63c7ae367150
raajv07 <rajagopal.vijayaraghavan@gmail.com>
2025-09-27 16:11:58 -0400

    Add files via upload
    For loop with a simple addition operation

---
+++ Loop_with_add.py
@@ -0,0 +1,9 @@
+my_name = "Juee"
+my_age = 8
+for age_counter in range(10):
+    new_age=my_age*age_counter
+    print(f"iteration number oe age counting value {age_counter}")
+    print(f"my age is {new_age}")
+    
+print("I am out of the loop")
+
