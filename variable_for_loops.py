9c092044409334fb1628413ea9004bfc9df5c9dc
raajv07 <rajagopal.vijayaraghavan@gmail.com>
2025-09-27 16:10:32 -0400

    Add files via upload
    Python for  Rookies, Variables and For Loops

---
+++ variable_for_loops.py
@@ -0,0 +1,17 @@
+# This is a code we wnet over to understand variables and For loops, we have string and integre variable 
+my_name = "sahasra"
+my_friend= "krishna"
+my_age=12
+
+print("This program teaches for loop. \n note indentations ")
+
+for times_counter in range(3):
+    
+    print(f"iteration number {times_counter}")
+    print(f"my name is {my_name}")
+    print(f"my friend's name is {my_friend}")
+    print(f"my age is {my_age}")
+    print("I am still inside the loop")
+
+print("I am out of the loop")
+
